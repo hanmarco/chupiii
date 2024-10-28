2bd4170581973a3692d4ccfc5d9e27539f9a075a
Marco <hsn103@gmail.com>
2024-10-28 22:45:23 +0900

    workbox 캐시 설정

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
 
-// 이 주석을 제거하지 마세요 - Workbox가 이 위치에 프리캐시 매니페스트를 주입합니다
-self.__WB_MANIFEST
-
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 
-// 기본 프리캐시 설정
+// Workbox가 프리캐시 매니페스트를 주입합니다
 precacheAndRoute(self.__WB_MANIFEST);
 
 // 책 이미지를 위한 커스텀 캐시 설정
